059ae920783de34f94c4b7344ca6847e6ad84542
Ben Cochrane <bencochrane1@me.com>
2017-09-10 21:20:03 +1000

    update for sns

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -3,21 +3,31 @@ import zipfile
 import StringIO
 import mimetypes
 
-def lambda_handler(event, context):    
-    s3 = boto3.resource('s3')
+def lambda_handler(event, context):
     
-    portfolio_bucket = s3.Bucket('portfolio.bencochrane.info')
-    build_bucket = s3.Bucket('portfoliobuild.bencochrane.info')
+    try:
+        s3 = boto3.resource('s3')
+        sns = boto3.resource('sns')
+        topic = sns.Topic('arn:aws:sns:us-east-1:502377633955:deployPortfolioTopic')
+        
+        portfolio_bucket = s3.Bucket('portfolio.bencochrane.info')
+        build_bucket = s3.Bucket('portfoliobuild.bencochrane.info')
+        
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')        
+        
+        topic.publish(Subject="Portfolio Deploy Success", Message="Successfully Deployed Portfolio")
     
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')        
+    except:
+        topic.publish(Subject="Portfolio Deploy FAILURE", Message="FAILED to deploy portfolio")
+        raise
     
     print "job done"
     return 'Hello from Lambda'
