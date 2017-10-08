1f9bb24e46103f92bb1e79214a33ccb138eeb16d
Ben Cochrane <bencochrane1@me.com>
2017-10-08 21:28:47 +1100

    add react components for portfolio

---
+++ __tests__/test-example-work.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExampleWork from '../js/example-work';
+
+describe("Example Work Component", () => {
+
+	it("should be a section element", () => {
+		
+	})
+});
--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-import 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log('loaded react-dom change more');
+const myWork = [
+	{
+		'title': "work example code",
+		'image': {
+			'desc': "work example code",
+			'src': "images/example1.png",
+			'comment': "code stuf"
+		}
+	},
+	{
+		'title': "work example colour",
+		'image': {
+			'desc': "work example colour",
+			'src': "images/example2.png",
+			'comment': "colour stuf"
+		}
+	},
+	{
+		'title': "work example cat",
+		'image': {
+			'desc': "work example cat",
+			'src': "images/example3.png",
+			'comment': "cat stuf"
+		}
+	}		
+]
+
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
 
